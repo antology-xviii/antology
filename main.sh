3ebc247e686576006afd8b9ff8abd9a5deef4ae7
artem <artem@AA5779.spb.edu>
2007-12-09 20:04:24 +0000

    metatagging fixed

--- main.sh
+++ main.sh
@@ -3,6 +3,13 @@
 TAGCOLL="$1"
 CONCEPTFILE="www/concept.html"
 
+echo "<html>"
+echo "<head>"
+echo "<!-- -head -->"
+echo "<title>Электронная антология русской литературы XVIII века</title>"
+echo "<!-- -middle -->"
+echo "</head>"
+echo "<body>"
 echo "<table>"
 echo "<tr><td valign=\"top\">"
 echo "<H2 align=left>Оглавление</H2>"
@@ -14,3 +21,7 @@ echo "<h2 align=right>Концепция</h2>"
 sed '/<!-- +headline -->/,/<!-- -headline -->/!d' $CONCEPTFILE
 echo "..."
 echo "<div align=right><a href=\"/cgi-bin/static.cgi/" $CONCEPTFILE "\">Читать текст концепции целиком &gt;&gt;</a></div>"
+
+echo "</table>"
+echo "<!-- +foot -->"
+echo "</html>"
