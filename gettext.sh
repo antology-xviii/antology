4c5afd954a95e273175d72dace5f9612a153a938
artem <artem@AA5779.spb.edu>
2007-12-10 23:12:42 +0000

    fixed images

--- gettext.sh
+++ gettext.sh
@@ -32,19 +32,22 @@ for p in `tagcoll reverse --remove-tags='!picture::*' sample.coll | tagcoll grep
         picture::leading::*)
         p="${p#picture::leading::}"
         name="${p%%::*}"
-        descr="${p##*::}"
+        descr="${p#*::}"
+        descr="${descr//@/&}"
         LEADING_PICTURES="$LEADING_PICTURES (\"$name\" . \"$descr\")"
         ;;
         picture::inline::*)
         p="${p#picture::inline::}"
         name="${p%%::*}"
-        descr="${p##*::}"
+        descr="${p#*::}"
+        descr="${descr//@/&}"
         INLINE_PICTURES="$INLINE_PICTURES (\"$name\" . \"$descr\")"
         ;;
         picture::trailing::*)
         p="${p#picture::trailing::}"
         name="${p%%::*}"
-        descr="${p##*::}"
+        descr="${p#*::}"
+        descr="${descr//@/&}"
         TRAILING_PICTURES="$TRAILING_PICTURES (\"$name\" . \"$descr\")"
         ;;
     esac
@@ -52,7 +55,7 @@ done
 
 SP_ENCODING=KOI8-R openjade -t sgml -bKOI8-R \
     -V"(define use-passport $PASSPORT)" -V"(define hilite-names $HILITE)" \
-    -V"(define leading-pictures '($LEADING_PICTURES)" \
-    -V"(define inline-pictures '($INLINE_PICTURES)" \
-    -V"(define trailing-pictures '($TRAILING_PICTURES)" \
+    -V"(define leading-pictures '($LEADING_PICTURES))" \
+    -V"(define inline-pictures '($INLINE_PICTURES))" \
+    -V"(define trailing-pictures '($TRAILING_PICTURES))" \
     -d mainconv.dssl ".$2"
