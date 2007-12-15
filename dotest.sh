60cef1711b48a23ef716cee613e023934005cfc6
artem <artem@iling.spb.ru>
2007-12-15 22:47:01 +0000

    test results page added

---
+++ dotest.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+echo "<html>"
+echo "<head>"
+echo "<!-- -head -->"
+echo "<title>Результаты теста</title>"
+echo "<!-- +middle -->"
+echo "</head>"
+echo "<body>"
+echo "<!-- -middle -->"
+
+read
+echo "$REPLY"
+
+echo "<!-- +foot -->"
+
