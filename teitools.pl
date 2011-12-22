a4f3c156a7b5f3a4e60bd11dd63767a6ede2c0df
artem <artem@AA5779.spb.edu>
2011-12-22 20:14:55 +0000

    fixed sdata mapping

--- teitools.pl
+++ teitools.pl
@@ -17,6 +17,62 @@ map_entity('[apos  ]', '\'').
 map_entity('[lt    ]', '<').
 map_entity('[gt    ]', '>').
 map_entity('[copy  ]', '\u00A9').
+map_entity('[numero]', '\u2116').
+map_entity('[aacute]', '\u00E1'). 
+map_entity('[Aacute]', '\u00C1'). 
+map_entity('[acirc ]',  '\u00E2'). 
+map_entity('[Acirc ]', '\u00C2'). 
+map_entity('[agrave]', '\u00E0'). 
+map_entity('[Agrave]', '\u00C0'). 
+map_entity('[aring ]',  '\u00E5'). 
+map_entity('[Aring ]', '\u00C5'). 
+map_entity('[atilde]', '\u00E3'). 
+map_entity('[Atilde]', '\u00C3'). 
+map_entity('[auml  ]', '\u00E4'). 
+map_entity('[Auml  ]', '\u00C4'). 
+map_entity('[aelig ]', '\u00E6'). 
+map_entity('[AElig ]', '\u00C6'). 
+map_entity('[ccedil]', '\u00E7'). 
+map_entity('[Ccedil]', '\u00C7'). 
+map_entity('[eth   ]', '\u00F0'). 
+map_entity('[ETH   ]', '\u00D0'). 
+map_entity('[eacute]', '\u00E9'). 
+map_entity('[Eacute]', '\u00C9'). 
+map_entity('[ecirc ]', '\u00EA'). 
+map_entity('[Ecirc ]', '\u00CA'). 
+map_entity('[egrave]', '\u00E8'). 
+map_entity('[Egrave]', '\u00C8'). 
+map_entity('[euml  ]', '\u00EB'). 
+map_entity('[Euml  ]', '\u00CB'). 
+map_entity('[iacute]', '\u00ED'). 
+map_entity('[Iacute]', '\u00CD'). 
+map_entity('[icirc ]', '\u00EE'). 
+map_entity('[Icirc ]', '\u00CE'). 
+map_entity('[igrave]', '\u00EC'). 
+map_entity('[Igrave]', '\u00CC'). 
+map_entity('[iuml  ]', '\u00EF'). 
+map_entity('[Iuml  ]', '\u00CF'). 
+map_entity('[ntilde]', '\u00F1'). 
+map_entity('[Ntilde]', '\u00D1'). 
+map_entity('[oacute]', '\u00F3'). 
+map_entity('[Oacute]', '\u00D3'). 
+map_entity('[ocirc ]', '\u00F4'). 
+map_entity('[Ocirc ]', '\u00D4'). 
+map_entity('[ograve]', '\u00F2'). 
+map_entity('[Ograve]', '\u00D2'). 
+map_entity('[oslash]', '\u00F8'). 
+map_entity('[Oslash]', '\u00D8'). 
+map_entity('[otilde]', '\u00F5'). 
+map_entity('[Otilde]', '\u00D5'). 
+map_entity('[ouml  ]', '\u00F6'). 
+map_entity('[Ouml  ]', '\u00D6'). 
+map_entity('[szlig ]', '\u00DF'). 
+map_entity('[thorn ]', '\u00FE'). 
+map_entity('[THORN ]', '\u00DE'). 
+map_entity('[uacute]', '\u00FA'). 
+map_entity('[Uacute]', '\u00DA'). 
+map_entity('[ucirc ]', '\u00FB'). 
+
 
 replace_sdata(sdata(Text), Result) :-
     map_entity(Text, Result).
